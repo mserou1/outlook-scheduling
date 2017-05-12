91d33a89ecdb1c2d0427f508b85f60fa1aefadf8
mserou1 <mserou1@gmail.com>
2017-05-12 14:52:45 -0500

    oauth

---
+++ utils/config.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+module.exports = {
+    creds: {
+        callbackURL: 'http://localhost:3000/token',
+        clientID: '5131b3dd-25a5-4ae8-a250-9e4ac4ca9398',
+        clientSecret: 'FrTnRSQwhT4qFuL9XgrZGYd',
+        identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
+        skipUserProfile: true,
+        responseType: 'code',
+        validateIssuer: false,
+        responseMode: 'query',
+        scope: ['User.Read', 'Calendars.Read']
+    }
+};
